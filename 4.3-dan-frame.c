cdd680ad9acf027ebe84d91beaa464a52b46f94b
ahmad <lidgnulinux@gmail.com>
2022-08-20 06:17:13 +0700

    Jendela berframe.

---
+++ 4.3-dan-frame.c
@@ -0,0 +1,41 @@
+#include <gtk/gtk.h>
+
+static void
+activate (GtkApplication* app,
+          gpointer        user_data)
+{
+  GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+  GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  GtkWidget *window;
+  GtkWidget *frame;
+  GtkWidget *label;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  // gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+  frame = gtk_frame_new("frame");
+  label = gtk_label_new ("Jendela berlabel dan berframe.");
+  gtk_container_add (GTK_CONTAINER (frame), label);
+  gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, FALSE, 0);
+
+  gtk_container_add(GTK_CONTAINER(vbox), hbox);
+  gtk_container_add(GTK_CONTAINER(window), vbox);
+
+  gtk_widget_show_all (window);
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+}
